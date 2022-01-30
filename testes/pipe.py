b91b23a6340a0eaf9db35ba85e33ff1139d78e9e
Isaias <isaiasramos_@outlook.com>
2022-01-30 02:08:24 -0300

    especificação de prefixo

--- testes/pipe.py
+++ testes/pipe.py
@@ -1,6 +1,11 @@
 import re
-heroRegex = re.compile(r'Batman|Tina Fey')
+"""heroRegex = re.compile(r'Batman|Tina Fey')
 mo1 = heroRegex.search('Batman and Tina Fey.')
 print(mo1.group())
 mo2 = heroRegex.search('Tina Fey and Batman.')
-print(mo2.group())
+print(mo2.group())"""
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')    # prefixo especificado
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+print(mo.group(1))
