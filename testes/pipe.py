363dc30941e97352d6ad4d8ef9d044602c55b7d4
Isaias <isaiasramos_@outlook.com>
2022-01-30 11:31:51 -0300

    correspondência opcional usando ponto de interrogação - usando numeros de telefone

--- testes/pipe.py
+++ testes/pipe.py
@@ -10,8 +10,15 @@ mo = batRegex.search('Batmobile lost a wheel')
 print(mo.group())
 print(mo.group(1))"""
 
-batRegex = re.compile(r'Bat(wo)?man')
+"""batRegex = re.compile(r'Bat(wo)?man')
 mo1 = batRegex.search('The Adventures of Batman')
 print(mo1.group())
 mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())"""
+
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+mo1 = phoneRegex.search('My number is 415-555-4242')
+print(mo1.group())
+mo2 = phoneRegex.search('My number is 555-4242')
 print(mo2.group())
+
