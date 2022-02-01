b7fc4964bbe684fd689683d8e5f8ee73d904db4d
Isaias <isaiasramos_@outlook.com>
2022-02-01 18:01:27 -0300

    Correspondendo a repetições específicas usando chaves (correspondencias greedy e nongreedy)

---
+++ testes/chaves.py
@@ -0,0 +1,18 @@
+import re
+
+haRegex = re.compile(r'(Ha){3}')
+mo1 = haRegex.search('HaHaHaHaHaHa')    # mo1 recebe apenas HaHaHa
+print(mo1.group())
+print(mo1)
+mo2 = haRegex.search('Ha')
+print(mo2 is None)      # como não há correspondência em 'Ha', search retorna None
+
+# correspondências greedy (gulosas) e nongreedy
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo2.group())
+# podemos verificar que o ponto de interrogação pode ter dois significados em regex:
+# declarar uma correspondência nongreedy ou indicar um grupo opcional
