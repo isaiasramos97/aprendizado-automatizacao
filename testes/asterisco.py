26100578f6f435abc21c122096be820cc8515763
Isaias <isaiasramos_@outlook.com>
2022-02-01 12:30:15 -0300

    Correspondendo a zero ou mais ocorrências usando asterisco

---
+++ testes/asterisco.py
@@ -0,0 +1,13 @@
+import re
+
+batRegex = re.compile(r'Bat(wo)*man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+mo3 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo3.group())
+
+# Batman: zero ocorrência de wo
+# batwoman: uma
+# batwowowowom: quatro
--- testes/pipe.py
+++ testes/pipe.py
@@ -16,9 +16,8 @@ print(mo1.group())
 mo2 = batRegex.search('The Adventures of Batwoman')
 print(mo2.group())"""
 
-phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+"""phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
 mo1 = phoneRegex.search('My number is 415-555-4242')
 print(mo1.group())
 mo2 = phoneRegex.search('My number is 555-4242')
-print(mo2.group())
-
+print(mo2.group())"""
